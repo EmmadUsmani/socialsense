c187456fba62c2656c696a846291c18d6cc245ea
Emmad Usmani <emmadusmani@berkeley.edu>
2025-03-16 12:36:30 -0700

    render results with mock data

--- client/src/pages/Demo/Demo.js
+++ client/src/pages/Demo/Demo.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Results from "../Search/Stages/Results/Results"
 
 export default function Demo() {
-  return <div>Demo</div>
+  const mockProps = {
+    loading: false,
+    hashtags: [{ photography: 0.85 }, { nature: 0.76 }, { landscape: 0.92 }],
+    profileImageSrc: "https://picsum.photos/150",
+    handle: "demouser",
+    file: {
+      src: "https://picsum.photos/800/600",
+    },
+    engagement: 245,
+    setHashtags: (newHashtags) => console.log("Updated hashtags:", newHashtags),
+  }
+
+  return <Results {...mockProps} />
 }
