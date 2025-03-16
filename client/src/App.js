94af606e9e75c9765ac360d1cd3b3303ab0fee87
Emmad Usmani <emmadusmani@berkeley.edu>
2025-03-16 12:05:02 -0700

    setup mock demo route in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,45 @@
-import React from 'react';
-import './App.css';
-
-
+import React from "react"
+import "./App.css"
 
 // Router
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom"
 
 // Pages
-import About from './pages/About/About';
-import Search from './pages/Search/Search';
-import Home from './pages/Home/Home';
+import About from "./pages/About/About"
+import Search from "./pages/Search/Search"
+import Home from "./pages/Home/Home"
+import Demo from "./pages/Demo/Demo"
 
 // Components
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import { connect } from 'react-redux';
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import { connect } from "react-redux"
 
 function App(props) {
-  return <div className={`App ${props.darkMode ? 'dark-mode' : null}`}>
-    <Navbar/>
-    <div 
-    className='page-wrapper fade-in-on-load'
-    style={{animationDelay: '2.4s', animationDuration: '.6s'}}>
+  return (
+    <div className={`App ${props.darkMode ? "dark-mode" : null}`}>
+      <Navbar />
+      <div
+        className="page-wrapper fade-in-on-load"
+        style={{ animationDelay: "2.4s", animationDuration: ".6s" }}
+      >
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/search" component={Search} />
+          <Route path="/demo" component={Demo} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
 
-      <Switch>
-        <Route path='/about' component={About}/>
-        <Route path='/search' component={Search}/>
-        <Route path='/' component={Home}/>
-      </Switch>
-      
+      <Footer />
     </div>
-    
-  <Footer/>
-  </div>
+  )
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    darkMode: state.theme.darkMode
+    darkMode: state.theme.darkMode,
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
